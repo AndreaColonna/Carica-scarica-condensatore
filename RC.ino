a169eb1dcfbb46b9dc78f4587c74193ebe9c94d8
AndreaColonna <96191719+AndreaColonna@users.noreply.github.com>
2022-05-13 15:50:54 +0200

    Update RC.ino

--- RC.ino
+++ RC.ino
@@ -13,11 +13,15 @@
   #define LED_PWR 25
 
   const int Sensor = A0;             //Pin di lettura del voltaggio
-  float SenVal = 0;            //variabile per la lettura del voltaggio
   int Supply = 8;              //Pin di carica e scarica del condensatore
-
-unsigned long initial_time, first_time, fourth_time;
-float first_difference_float, fourth_difference_float;   // variabili di comodo per il tempo
+  float first_difference_float, fourth_difference_float;   // variabili di comodo per il tempo
+  unsigned long tempo_iniziale;
+  unsigned long tempo;
+  float tensione;
+  float resistenza = 10000.0;
+  float capacita = 0.0001;
+  float tau;
+  int passi = 100;
 
 void setup() {
   PhyphoxBLE::minConInterval = 6; //6 = 7.5ms       // impostazioni per il bluetooth
@@ -33,13 +37,14 @@ void setup() {
   digitalWrite(VERDE, HIGH);
   digitalWrite(BLU, HIGH);
   
-  Serial.begin(9600);  // inizializza il monitor seriale nel caso si utilizza phyphox il monitor seriale deve essere escluso
+ // Serial.begin(9600);  // inizializza il monitor seriale nel caso si utilizza phyphox il monitor seriale deve essere escluso
  
   pinMode(Supply, OUTPUT);      //Dichiarazione del pin 8 di carica e scarica
   digitalWrite(Supply, LOW);    //voltaggio 0 V sul pin di carica
-  
+  tau = resistenza * capacita;
   PhyphoxBLE::start(board_name); // Il nome della scheda può essere cambiato all'inizio del programma, attiva la conversazione bluetooth
-  PhyphoxBLE::configHandler = &receivedData; // routine per mandare i comandi sulla scheda 
+  PhyphoxBLE::configHandler = &receivedData; // routine per mandare i comandi sulla scheda
+  delay(1000);
 }
 
 void loop() {
@@ -62,18 +67,32 @@ void receivedData() { // see Phyphox Arduino Library example
 }
 
 void CaricaScarica(){
-   first_time = millis();    // leggi il tempo di misura
-   first_difference_float = ((float)first_time - (float)initial_time) / 1000;  // tempo a cui viene la lettura
-   SenVal = analogRead(Sensor);      //Lettura del valore di tensione del condensatoreSample 
-   Serial.print(SenVal*3.3/1023.);   // stampa su monitor seriale latensione letta normalizzata a 3.3 V 
-   Serial.print(" ");                // stampa spazio vuoto 
-   Serial.println(first_difference_float);  // stampa il tempo normalizzato
-   PhyphoxBLE::write(first_difference_float, SenVal);
-   delay(2);
-   if (SenVal == 1023){            // se il valore di tensione è al massimo metti al tensione a 0 V
-    digitalWrite(Supply, LOW);
+  // *************Carico del condensatore
+ digitalWrite(Supply,HIGH);   // sto caricando il condensatore
+ tempo_iniziale = micros();
+ // 
+ for (int i = 0; i < passi; i ++) {       // campiono il tempo e il voltaggio in 100 passi
+  tempo = micros();
+  tensione = analogRead(A0)*3.3/1023.;
+  first_difference_float = (tempo - tempo_iniziale )/1000000.0;
+   PhyphoxBLE::write(first_difference_float, tensione);
+   //Serial.print(tensione);
+  //Serial.print(" ");
+  //Serial.println((tempo - tempo_iniziale )/1000000.0);
+  delay(int(tau*100));
   }
-   if(SenVal == 0){
-   digitalWrite(Supply, HIGH);  // se il valore di tensione è al minimo metti al tensione a 3.3 V
-   }
-   }
+  //******** Scarico del condensatore
+ digitalWrite(Supply, LOW);       // scarico il condensatore
+ tempo_iniziale = micros();
+  //
+  for (int i = 0; i < passi; i ++) {       // campiono il tempo e il voltaggio in 100 passi
+  tempo = micros();
+  tensione = analogRead(A0)*3.3/1023.;
+  first_difference_float = (tempo - tempo_iniziale )/1000000.0;
+  PhyphoxBLE::write(first_difference_float, tensione);
+   //Serial.print(tensione);
+  //Serial.print(" ");
+  //Serial.println((tempo - tempo_iniziale )/1000000.0);
+  delay(int(tau*100));
+  }
+ }
